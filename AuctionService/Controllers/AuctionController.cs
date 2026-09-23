c229e748fa6d09a3a86e6fcf56b462206cc0fda1
Ben <loperb06@gmail.com>
2026-09-22 22:57:00 -0400

    Initial commit

---
+++ AuctionService/Controllers/AuctionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuctionController : Controller
+{
+    // GET
+    public ActionResult<string> Index()
+    {
+        return "Hi (:";
+    }
+}
